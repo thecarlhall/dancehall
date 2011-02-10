067e96fcbd5630dff151864f4f02ad2c94886a1a
Carl Hall <carl@hallwaytech.com>
2011-02-09 22:56:22 -0500

    Added base php file for dance party plugin. Changed license from Apache to GPLv2 to match other plugins. Moved opensocial services to their own folder. Created folder for danceparty files.

---
+++ bp-danceparty.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Dance Party
+Plugin URI: http://github.com/thecarlhall/danceparty
+Description: OpenSocial service implementations for BuddyPress.
+Version: 0.1
+Author: Carl Hall
+Author URI: http://about.me/thecarlhall
+License: GPL2
+
+Copyright 2010 Hallway Technologies (http://hallwaytech.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Dance Party === 
+Contributors: Carl Hall <carl@hallwaytech.com>
+Tags: opensocial, buddypress, social networking
+Requires at least: 3.1
+Tested up to: 3.1
+Stable tag: 0.1
+Requires: BuddyPress 1.3
+
+== Description ==
+
+Implementations of OpenSocial services using BuddyPress functionality.
